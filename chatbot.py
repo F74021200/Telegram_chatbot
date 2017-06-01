cbcb369bc12f6174552886eec40d65ea82898c0a
F74021200 <gzxc3kc4690@gmail.com>
2017-06-01 16:23:46 +0800

    Add the part of area to go from and to

--- chatbot.py
+++ chatbot.py
@@ -7,7 +7,14 @@ from selenium import webdriver
 from selenium.webdriver.support.ui import Select
 from selenium.webdriver import Firefox
 
-tran1 = None
+tran1 = None #insert "traintime"
+tran2 = None #insert "north"、"mid"、 "south"、"east" from which to go
+tran3 = None #insert "north"、"mid"、 "south"、"east" to which
+tran4 = None #insert the area to go
+trainfrom = None
+trainto = None
+partfrom = None
+partto = None
 
 app = Flask(__name__)
 bot = telegram.Bot(token='354914266:AAHNeiSma3dDA94TehenEFUPDP9LiHcUAEE')
@@ -24,24 +31,79 @@ def webhook_handler():
 		update = telegram.Update.de_json(request.get_json(force=True), bot)
 
 		text = update.message.text
-
+		global tran1
+		global tran2
+		global tran3
+		global tran4
+		global trainfrom
+		global trainto
 #		update.message.reply_text(text)
 		if 'traintime' in text:
 			tran1 = 1
-		elif 'check' in text:
+			update.message.reply_text("What part do you go from,\'north\'、\'mid\'、\'south\'、\'east\'?")
+		elif tran1 == 1:
+			if 'north' in text:
+				partfrom = 'FromStationList1'
+			if 'mid' in text:
+				partfrom = 'FromStationList2'
+			if 'south' in text:
+				partfrom = 'FromStationList3'
+			if 'east' in text:
+				partfrom = 'FromStationList4'
+#update.message.reply_text(partfrom)
+			update.message.reply_text("What part do you go to,\'north\'、\'mid\'、\'south\'、\'east\'?")
+			tran2 = 1
+			tran1 = 2
+		elif tran2 == 1:
+			if 'north' in text:
+				partto = 'FromStationList1'
+			if 'mid' in text:
+				partto = 'FromStationList2'
+			if 'south' in text:
+				partto = 'FromStationList3'
+			if 'east' in text:
+				partto = 'FromStationList4'
+#update.message.reply_text(partto)
+			update.message.reply_text("What name of area do you go from?")
+			tran3 = 1
+			tran2 = 2
+			tran1 = 3
+		elif tran3 == 1:
+			trainfrom = text
+#update.message.reply_text(trainfrom)
+			update.message.reply_text("What name of area do you go to?")
+			tran4 = 1
+			tran3 = 2
+			tran2 = 3
+			tran1 = 4
+		elif tran4 == 1:
+			trainto = text
+			update.message.reply_text(trainfrom)
+#update.message.reply_text(trainto)
 			timesearch()
+		elif 'exit' in text:
+			tran1 = 0
+			tran2 = 0
+			tran3 = 0
+			tran4 = 0
+			trainfrom = None
+			trainto = None
+			partfrom = None
+			partto = None
 		else:
-			update.message.reply_text("What do you test?")
+			update.message.reply_text("I don't know what you mean,please tell me again or reply 'exit' for other talk topic.")
 	return 'ok'
 def timesearch():
+	global trainfrom
+	global trainto
 	driver = webdriver.Firefox()
 	driver.get("http://twtraffic.tra.gov.tw/twrail/EasySearch.aspx")
 			
 	selectfrom = Select(driver.find_element_by_name('FromStationList1'))
-	selectfrom.select_by_visible_text(u"福隆")
+	selectfrom.select_by_visible_text(trainfrom)
 
 	selectto = Select(driver.find_element_by_name('ToStationList1'))
-	selectto.select_by_visible_text(u"雙溪")
+	selectto.select_by_visible_text(trainto)
 
 	button = driver.find_element_by_id("Button2")
 	button.click()
